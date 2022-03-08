c10b41928e258d2cad27637e1c82de3667b55982
ggichuru <ggichuru.dev@gmail.com>
2022-03-08 19:05:48 +0300

    Deploy scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import { WAKULIMA_NFT_ADDRESS } from "../constants";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const WakulimaToken = await ethers.getContractFactory("WakulimaToken");
+  const wakulimaToken = await WakulimaToken.deploy(WAKULIMA_NFT_ADDRESS);
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await wakulimaToken.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Wakulima deployed to:", wakulimaToken.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
